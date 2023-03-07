aa45a4e259734d2bf1954eed177b77f14a72209f
JasonPhan1 <98352769+JasonPhan1@users.noreply.github.com>
2023-03-07 00:38:46 -0600

    Update company.cpp

--- CS303 Assignment2/company.cpp
+++ CS303 Assignment2/company.cpp
@@ -7,12 +7,15 @@ Employee::Employee(string firstname, string lastname) {
 }
 
 double Employee::weeklySalary(int hoursworked, int hourlyrate) {
+    return hoursworked;
 }
 
 int Employee::healthCare(int paycheck) {
+    return paycheck;
 }
 
 int Employee::vacationDays(int hoursworked) {
+    return hoursworked;
 }
 
 // Nonprofessional
@@ -24,10 +27,12 @@ double NonProfessionalEmployee::weeklySalary(int hoursworked, int hourlyrate) {
 
 int NonProfessionalEmployee::healthCare(int paycheck) {
     cout << "With the employee making " << paycheck << ", you will have " << (paycheck * .20) << " dollars deducted for health care.\n"; // more healthcare cost 
+    return paycheck;
 }
 
 int NonProfessionalEmployee::vacationDays(int hoursworked) {
     cout << "Since you are a nonprofessional employee, you get a 3 day vacation every 3 months.\n"; // random amount
+    return hoursworked;
 }
 
 // Professional
@@ -39,8 +44,10 @@ double ProfessionalEmployee::weeklySalary(int hoursworked, int hourlyrate) {
 
 int ProfessionalEmployee::healthCare(int paycheck) {
     cout << "With the employee making " << paycheck << ", you will have " << (paycheck * .1) << " dollars deducted for health care.\n";
+    return paycheck;
 }
 
 int ProfessionalEmployee::vacationDays(int hoursworked) {
     cout << "Since you are a professional employee, you get a 1 week vacation every 3 months.\n";
+    return hoursworked;
 }
